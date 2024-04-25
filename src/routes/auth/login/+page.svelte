a015653b4a19803e88d02c44f890511acc416e09
Sirshak Bohara <boharasirshak101@gmail.com>
2024-04-25 17:59:26 +0545

    feat: renders login form on page load

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,50 +1,7 @@
 <script lang="ts">
-	import CardWrapper from "$lib/components/auth/card-wrapper.svelte";
-	// import { superForm } from "sveltekit-superforms";
-
-	// export let data;
-
-	// const { form, errors, constraints, message, enhance } = superForm(data.form);
+	import LoginForm from "$lib/components/auth/login-form.svelte";
+	import type { PageData } from "./$types.js";
+	export let data: PageData;
 </script>
 
-<CardWrapper
-	headerLabel="Welcome Back"
-	backButtonLabel="Don't have an account?"
-	backButtonHref="/auth/signup"
-	showSocial
->
-	<!-- {#if $message}<h3>{$message}</h3>{/if}
-
-	<form use:enhance method="POST">
-		<label for="email">Email</label>
-		<input
-			type="text"
-			name="email"
-			bind:value={$form.email}
-			aria-invalid={$errors.email ? "true" : undefined}
-			{...$constraints.email}
-		/>
-		{#if $errors.email}<span class="invalid">{$errors.email}</span>{/if}
-
-		<label for="password">Password</label>
-		<input
-			type="password"
-			name="password"
-			bind:value={$form.password}
-			aria-invalid={$errors.password ? "true" : undefined}
-			{...$constraints.password}
-		/>
-		{#if $errors.password}<span class="invalid">{$errors.password}</span>{/if}
-
-		<input type="submit" value="Login" />
-
-		<a href="/auth/signup">Don't have an account?</a>
-		<a href="/auth/forget-password">Forget password?</a>
-	</form> -->
-</CardWrapper>
-
-<!-- <style>
-	.invalid {
-		color: red;
-	}
-</style> -->
+<LoginForm data={data.form} />
