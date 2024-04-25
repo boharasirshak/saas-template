046591b7afa1632223f96fb18e65df282d5d344d
Sirshak Bohara <boharasirshak101@gmail.com>
2024-04-25 17:59:47 +0545

    feat: using global schema

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -1,16 +1,11 @@
+import { loginSchema } from "$lib/schemas";
 import type { Actions } from "@sveltejs/kit";
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import { z } from 'zod';
 import { signIn } from "../../../auth";
 
-const schema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
-});
-
 export const load = (async () => {
-  const form = await superValidate(zod(schema));
+  const form = await superValidate(zod(loginSchema));
   return { form };
 });
 
